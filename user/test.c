fdcb2a0f9144d86476a23277750f94a0d172640d
Haylee Jane Monteiro <haylee.jane3@gmail.com>
2016-11-22 18:32:12 -0600

    adding a lock test

--- user/test.c
+++ user/test.c
@@ -8,6 +8,7 @@
 
 int ppid;
 int global = 1;
+lock_t lock;
 
 #define assert(x) if (x) {} else { \
    printf(1, "%s: %d ", __FILE__, __LINE__); \
@@ -23,16 +24,28 @@ int
 main(int argc, char *argv[])
 {
    ppid = getpid();
+   lock_init(&lock);
 
    int arg = 35;
+   sleep(20);
+  lock_acquire(&lock);
+  sleep(10);
+  lock_release(&lock);
+
    int thread_pid1 = thread_create(worker, &arg);
    printf(1, "Created thread 1. PID : %d\n\n", thread_pid1);
+
+   sleep(60);
+  lock_acquire(&lock);
 
    int thread_pid2 = thread_create(worker, &arg);
    printf(1, "Created thread 2. PID : %d\n\n", thread_pid2);
 
    int thread_pid3 = thread_create(worker, &arg);
    printf(1, "Created thread 3. PID : %d\n\n", thread_pid3);
+
+   sleep(80);
+   lock_release(&lock);
 
    int thread_pid4 = thread_create(worker, &arg);
    printf(1, "Created thread 4. PID : %d\n\n", thread_pid4);
@@ -67,9 +80,11 @@ main(int argc, char *argv[])
 
 void
 worker(void *arg_ptr) {
+   lock_acquire(&lock);
    int arg = *(int*)arg_ptr;
    assert(arg == 35);
    //assert(global == 1);
    global++;
+   lock_release(&lock);
    exit();
 }
